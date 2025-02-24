0738ccf618bd1610af8017d5838f5a11c431de1d
rabinapanta <rabinapanta999@gmail.com>
2025-02-24 17:10:22 -0500

    add hero

---
+++ src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./Hero.module.css"
+
+export const Hero = () => {
+  return <section className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Hi, I'm Rabina</h1>
+      <p className={styles.description}>I'm a fresh Graduate from Lambton College with my Post-Graduate Degree in Full Stack Software Development. I'm actively looking for an entry-level IT roles to start my career. Reach out if you'd like to learn more!</p>
+      <a href="mailto:rabinapanta78@gmail.com" className={styles.contactBtn}>Contact Me</a>
+    </div>
+    <img src={getImageUrl("hero/me.jpg")} alt="Hero Image of me" className={styles.heroImg}/>
+    <div className= {styles.topBlur} />
+    <div className= {styles.buttomBlur} />
+   
+  </section>
+}
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,8 @@ export const Navbar = () => {
                     alt="menu-button"
                     onClick={() => setMenuOpen(!menuOpen)}
                 />
-                <ul className= {styles.menuItems}>
+                <ul className= {`${styles.menuItems} ${menuOpen &&  styles.menuOpen}`} 
+                onClick={() => setMenuOpen(false)}>
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experiences</a></li>
                     <li><a href="#projects">Accomplishments</a></li>
