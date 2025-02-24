e490b2d68d897595addc1135f9b0c16240ee64b9
rabinapanta <rabinapanta999@gmail.com>
2025-02-24 14:47:46 -0500

    add menuIcons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Navbar.module.css";
+import {getImageUrl} from "../../utils";
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className= {styles.navbar}>
             <a className= {styles.title} href="/">Portfolio</a>
             <div className= {styles.menu}>
+                <img 
+                    className={styles.menuBtn} 
+                    src={
+                    menuOpen 
+                        ? getImageUrl("assets/nav/closeIcon.png")
+                        : getImageUrl("assets/nav/menuIcon.png")
+                    } 
+                    alt="menu-button"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                />
                 <ul className= {styles.menuItems}>
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experiences</a></li>
