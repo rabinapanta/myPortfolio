0d12e6b8f51a2510f78c3adeade10d1281296e39
rabinapanta <rabinapanta999@gmail.com>
2025-02-27 23:00:14 -0500

    Add experiences

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
                 onClick={() => setMenuOpen(false)}>
                     <li><a href="#about">About</a></li>
                     <li><a href="#experience">Experiences</a></li>
-                    <li><a href="#projects">Accomplishments</a></li>
+                    <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
