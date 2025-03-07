3fbad21eaac30d5274ec926e0024f6fb6245a321
rabinapanta <rabinapanta999@gmail.com>
2025-03-07 15:31:27 -0500

    undo getImageUrl update

--- src/utils.js
+++ src/utils.js
@@ -2,10 +2,10 @@
 //     return new URL (`/assets/${path}`, import.meta.url).href;
 // };
 
-// export const getImageUrl = (path) => {
-//     return `/assets/${path}`;
-// };
-
 export const getImageUrl = (path) => {
-    return `${import.meta.env.BASE_URL}assets/${path}`;
-  };
+    return `/assets/${path}`;
+};
+
+// export const getImageUrl = (path) => {
+//     return `${import.meta.env.BASE_URL}assets/${path}`;
+//   };
