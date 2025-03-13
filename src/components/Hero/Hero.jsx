79ba450f17ba34a7bbe639dc4dadf8936926ca0b
rabinapanta <rabinapanta999@gmail.com>
2025-03-13 16:12:10 -0400

    comment title

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
   return (
   <section className={styles.container}>
     <div className={styles.content}>
-      <h1 className={styles.title}>Hi, I'm Rabina</h1>
+      {/* <h1 className={styles.title}>Hi, I'm Rabina</h1> */}
       <p className={styles.description}>Hello, I'm Rabina, a recent graduate from Lambton College with a Post-Graduate Certificate in Full Stack Software Development. I am actively seeking entry-level opportunities in the IT industry to apply my skills and grow professionally. Feel free to connect if you'd like to learn more about me!</p>
       <a href="mailto:rabinapanta78@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
