9f9d52b97044d3a377b3c8314c2ac9dd81fb17f1
rabinapanta <rabinapanta999@gmail.com>
2025-03-17 17:15:38 -0400

    fix hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
   <section className={styles.container}>
     <div className={styles.content}>
       <h1 className={styles.title}>Hi, I'm Rabina</h1>
-      <p className={styles.description}>Hello, I'm Rabina, a recent graduate from Lambton College with a Post-Graduate Certificate in Full Stack Software Development. I am actively seeking entry-level opportunities in the IT industry to apply my skills and grow professionally. Feel free to connect if you'd like to learn more about me!</p>
+      <p className={styles.description}>I'm a recent graduate from Lambton College with a Post-Graduate Certificate in Full Stack Software Development. I am actively seeking entry-level opportunities in the IT industry to apply my skills and grow professionally. Feel free to connect if you'd like to learn more about me!</p>
       <a href="mailto:rabinapanta78@gmail.com" className={styles.contactBtn}>Contact Me</a>
     </div>
     <img src={getImageUrl("me/me.png")} alt="Hero Image of me" className={styles.heroImg} />
